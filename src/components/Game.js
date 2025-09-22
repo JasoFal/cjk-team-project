e85df093b78644d85fc77d2e34aa9e5d5e457221
Samantha Callie <Hoomee90@gmail.com>
2025-09-22 16:23:37 -0700

    add logic for conditional game option rendering

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,8 +6,32 @@ function Game({ currentScene, inventory, onChoice, }) {
   if (!currentScene) {
     return <div>Game Over!</div>; // TODO: add restart
   }
+
+  const validOption = (conditions) => {
+    let valid = true;
+    if (conditions) {
+      conditions.forEach(condition => {
+        switch (condition[0]) {
+          case "HAS":
+            if (!inventory.some((item) => item.id === condition[1])) {
+              valid = false;
+            }
+            break;
+          case "LACKS":
+            if (inventory.some((item) => item.id === condition[1])) {
+              valid = false;
+            }
+            break;
+        }
+      });
+    }
+    return valid;
+  }
+
   let sceneText = currentScene.text;
-  let sceneOptions = currentScene.options;
+  const sceneOptions = currentScene.options.reduce((acc, currVal) => {
+    return validOption(currVal.conditions) ? acc.concat(currVal) : acc;
+  }, []);
 
   return (
     <React.Fragment>
