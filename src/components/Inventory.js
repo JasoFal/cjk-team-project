8085da58b24107f8746e398a7b6660320fabd3e7
Samantha Callie <Hoomee90@gmail.com>
2025-09-30 13:17:51 -0700

    make inventory not display items of type "flag" and remove restart option from scene 1

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Item from './Item';
 
 function Inventory({ content }) {
-
+  // return unordered list element of all non-flag items
   return (
     <React.Fragment>
       <ul className="list-group d-inline-flex">
-        {content.map((item) =>
-          <Item key={item.id} item={item} />
-        )}
+        {content
+          .filter((item) => item.type !== "flag")
+          .map((item) =>
+            <Item key={item.id} item={item} />
+          )}
       </ul>
     </React.Fragment>
   );
