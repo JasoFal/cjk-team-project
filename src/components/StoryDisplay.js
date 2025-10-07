1b419ab95eb80afb19baa1757a9f04d8a2ea8991
Jason Falk <jasonfalk9@gmail.com>
2025-10-07 11:22:57 -0700

    add scroll png textbox for event display.

--- src/components/StoryDisplay.js
+++ src/components/StoryDisplay.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import './../component css/StoryDisplay.css';
+import scrollBg from './../assets/scroll-bg.png'
 
-function StoryDisplay({ text, image }) {
+function StoryDisplay({ newText, newImage }) {
+  console.log(newText, "input");
+  const [eventLog, setEventLog] = useState([]);
+  const logEndRef = useRef(null);
+  console.log(eventLog);
 
+  useEffect(() => {
+    if (newText || newImage) {
+      const newEvent = {};
+      if (newText) newEvent.text = newText;
+      if (newImage) newEvent.image = newImage;
+      setEventLog(prev => [...prev, newEvent]);
+    }
+  }, [newText, newImage]);
+
+  useEffect(() => {
+    if (logEndRef.current) {
+      logEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [eventLog]);
 
   return (
     <React.Fragment>
-      <p>{text}</p>
+      <div className='container'>
+        <img src={scrollBg} alt='scroll' className='scroll-image'/>
+        <div className='textbox'>
+          {eventLog.map((event, index) => (
+            <div key={index} className='event'>
+              {event.text && <div className='text'>{event.text}</div>}
+              {event.image && (
+                <img src={event.image} className='image' alt='' onError={(e) => e.target.style.display = 'none'} />
+              )}
+            </div>
+          ))}
+          <div ref={logEndRef} />
+        </div>
+      </div>
     </React.Fragment>
   )
 }
