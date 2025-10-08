ae49476acd5b0d23d13fce2dae02802a279e52ef
Jason Falk <jasonfalk9@gmail.com>
2025-10-08 11:15:39 -0700

    Add a background table image.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import GameControl from './components/GameControl'
+import GameControl from './components/GameControl';
+import './component css/Background.css';
 
 function App() {
-
   return (
     <React.Fragment>
-      <div className="container ">
-        <h1>Text Adventure</h1>
+      <div className="container">
+  <h1 className="main-title">Text Adventure</h1>
         <GameControl />
       </div>
     </React.Fragment>
