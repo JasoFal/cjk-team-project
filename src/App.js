6a589f9e0f8df5106f6bda3ec5163b6023fcf3db
Jason Falk <jasonfalk9@gmail.com>
2025-10-08 13:21:46 -0700

    Add ambient tavern music

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import tavernMusic from './assets/Bg-tavern-music.mp3';
 import GameControl from './components/GameControl';
 import './component css/Background.css';
 
 function App() {
   return (
     <React.Fragment>
+      <audio src={tavernMusic} autoPlay loop volume={0.5} style={{ display: 'none' }} />
       <div className="container">
         <h1 className="main-title">Text Adventure</h1>
         <GameControl />
